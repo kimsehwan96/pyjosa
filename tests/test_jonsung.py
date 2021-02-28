2c9e4bd2fb3fbfec10c6a349303e6e4286aa4e1f
Kimsehwan96 <sktpghks138@gmail.com>
2021-03-01 03:04:03 +0900

    add more directories

---
+++ tests/test_jonsung.py
@@ -0,0 +1,29 @@
+from pyjosa.jonsung import is_hangle, has_jongsung
+import unittest
+
+class HangleTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.kor = [ 
+         '한글',
+         '안녕',
+         '하윙',
+         '테스트케이스',
+         'enflish영어'
+        ]
+        self.not_kor = [
+            'english',
+            '영어eng',
+            '132r4eng',
+            'dinvf,5'
+        ]
+
+    def test_line_count(self):
+        for v in self.kor:
+            self.assertTrue(is_hangle(v))
+        
+        for v in self.not_kor:
+            self.assertFalse(is_hangle(v))
+
+if __name__ == '__main__':
+    unittest.main()

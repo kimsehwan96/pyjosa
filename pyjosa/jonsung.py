96d40bd9cc867226d5734a875a742cd4e47326c8
Kimsehwan96 <sktpghks138@gmail.com>
2021-03-01 01:29:04 +0900

    add checking hangle logic

---
+++ pyjosa/jonsung.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+import re
+
+J_LIST = [] # 종성 리스트  추가
+
+def is_hangle(string: str) -> bool:
+    last_char = string[-1]
+    if re.match('.*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*', last_char) is None:
+        return False
+
+
+
+
+def has_jungsung(string: str)  -> bool:
+    last_char = string[-1]
+    encoded_last_char = last_char.encode('utf8')
+    #유니코드 계산을 통해 종성을 분리한다.
+    #종성이 있을경우 True, 없을경우 False를 반환하도록 작성한다.
---
+++ pyjosa/josa.py
@@ -0,0 +1,1 @@
+from pyjosa.jonsung import is_hangle, has_jungsung

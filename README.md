5db956a4c1a97079cb0ea7af6c93ae751b66c10e
Kimsehwan96 <sktpghks138@gmail.com>
2021-03-01 01:05:55 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# For now it just test
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools  import setup,  find_packages
+
+setup(
+    name = 'pyjosa',
+    version = '0.0.1',
+    description = 'now it is test',
+    author = 'sehwan.kim',
+    author_email = 'sehwan.kim@ingkle.com',
+    url = '추가해야함',
+    packages = find_packages(exclude=[]),
+    license='MIT',
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.7'
+    ],
+    package_data = {},
+    zip_safe=False,
+    keyword = ['pypi test'],
+
+)

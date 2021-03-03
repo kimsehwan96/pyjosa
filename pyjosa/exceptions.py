e4644198794232e0aea536e1dd82422ad349f83e
Kimsehwan96 <sktpghks138@gmail.com>
2021-03-04 00:42:11 +0900

    add custom exception

--- pyjosa/exceptions.py
+++ pyjosa/exceptions.py
@@ -1,3 +1,7 @@
 class NotHangleException(Exception):
     def __init__(self):
         super().__init__("마지막 글자가 한글이 아닙니다.")
+
+class JosaTypeException(Exception):
+    def __init__(self):
+        super().__init__("메서드의 인자로 주어진 조사가 올바르지 않습니다.")
